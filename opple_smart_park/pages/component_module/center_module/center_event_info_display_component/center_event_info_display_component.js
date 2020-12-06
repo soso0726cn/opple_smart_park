7f0899ffb30f9e9ab3dc059dfb31ee7aafa4db8a
cenchen <cenchen@pateo.com.cn>
2020-12-07 00:23:31 +0800

    V6.0

--- opple_smart_park/pages/component_module/center_module/center_event_info_display_component/center_event_info_display_component.js
+++ opple_smart_park/pages/component_module/center_module/center_event_info_display_component/center_event_info_display_component.js
@@ -31,6 +31,12 @@ Component({
           imageUrl:null
         }
       )
+    },
+    tapImage:function(){
+      wx.previewImage({
+        current: this.data.imageUrl, // 当前显示图片的http链接
+        urls: [this.data.imageUrl] // 需要预览的图片http链接列表
+      });
     }
   },
 
